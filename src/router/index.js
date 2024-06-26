e1cea38592f609eddc265787f6d111656dc53ffa
Loren Rodriguez <rodriguezloren431@gmail.com>
2024-06-26 17:41:33 -0500

    Mostrando dinámicamente los productos

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',      
+      path: '/',  
+      redirect: '/pan'   
     },
     {
-      path: '/:categoria'
+      path: '/:categoria',
       name:"productos",
       component:() => import("@/views/ProductosView.vue"),
       props:true
